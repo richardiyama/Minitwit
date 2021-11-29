8808dae0c7b9d2f233cbef4a7f5f41879195ba1e
Richard Iyama <richardiyama@yahoo.com>
2021-11-29 11:25:21 +0100

    page flow image added

--- twit-client-app/src/http-common.js
+++ twit-client-app/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://stormy-mesa-92667.herokuapp.com",
+  baseURL: "https://stormy-mesa-92667.herokuapp.com/api/twit",
   headers: {
     "Content-type": "application/json"
   }
--- twit-client-app/src/services/auth.service.js
+++ twit-client-app/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://stormy-mesa-92667.herokuapp.com/api/auth/";
 
 class AuthService {
   login(email, password) {
